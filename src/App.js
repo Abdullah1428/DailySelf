f38ed891bd4a5097a05a950bb79d8e825af20993
Abdullah1428 <abdullahcse1428@gmail.com>
2022-05-07 17:34:22 +0200

    started dailyself project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Tone Analyzer</h1>
+      <h1 className='text-center my-3 p-3'>Welcome to Daily Self-Analyzer</h1>
+      <h2 className='my-3 p-3'>How are you feeling today?</h2>
     </div>
   );
 }
